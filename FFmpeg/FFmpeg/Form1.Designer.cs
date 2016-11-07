c574a3311074fdab56e426e0f14e689306637852
michauxquan <79365450@qq.com>
2016-11-07 18:08:22 +0800

    [F]Mater

---
+++ FFmpeg/FFmpeg/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FFmpeg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.labpath = new System.Windows.Forms.Label();
+            this.textpath = new System.Windows.Forms.TextBox();
+            this.folder1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(310, 17);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "选择";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // labpath
+            // 
+            this.labpath.AutoSize = true;
+            this.labpath.Location = new System.Drawing.Point(23, 22);
+            this.labpath.Name = "labpath";
+            this.labpath.Size = new System.Drawing.Size(65, 12);
+            this.labpath.TabIndex = 1;
+            this.labpath.Text = "文件路径: ";
+            // 
+            // textpath
+            // 
+            this.textpath.Location = new System.Drawing.Point(90, 19);
+            this.textpath.Multiline = true;
+            this.textpath.Name = "textpath";
+            this.textpath.ReadOnly = true;
+            this.textpath.Size = new System.Drawing.Size(205, 21);
+            this.textpath.TabIndex = 2;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(310, 52);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "处理水印";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(90, 137);
+            this.text2.Multiline = true;
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(205, 149);
+            this.text2.TabIndex = 4;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(310, 86);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "添加字幕";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(310, 118);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "动态文字";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(310, 150);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 23);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "提取图片";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(310, 180);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 23);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "图片视频";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(310, 210);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 23);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "图片处理";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 330);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.textpath);
+            this.Controls.Add(this.labpath);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label labpath;
+        private System.Windows.Forms.TextBox textpath;
+        private System.Windows.Forms.FolderBrowserDialog folder1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+    }
+}
+
---
+++ FFmpeg/FFmpeg/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFmpeg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        { 
+            folder1.Description = "请选择文件夹";
+            if (folder1.ShowDialog() == DialogResult.OK)
+            {
+                this.textpath.Text = folder1.SelectedPath;
+            } 
+        }
+        public string FFmpegPath { get; set; }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            string para = "";
+            if (string.IsNullOrEmpty(this.textpath.Text))
+            {
+                MessageBox.Show("请选择文件");
+                return;
+            }
+            FFmpegPath = "C:/ffmpeg/bin/ffmpeg.exe";
+            para =
+                "-i D:/Michaux/FFTest/rain.mp4 -i D:/Michaux/FFTest/egg.png -filter_complex \"overlay=10:10\" -b 1024k -acodec copy D:/Michaux/FFTest/aaa.mp4";
+            this.text2.Text= this.text2.Text+RunProcess(para);
+        }
+
+
+        //添加背景音乐 -i input_video.mp4 -i looped_audio.mp3 -shortest output_video.mp4
+
+    
+        /// <summary>
+        /// 调用ffmpeg.exe 执行命令
+        /// </summary>
+        /// <param name="Parameters">命令参数</param>
+        /// <returns>返回执行结果</returns>
+        private string RunProcess(string Parameters)
+        {
+            //创建一个ProcessStartInfo对象 并设置相关属性
+            //var oInfo = new ProcessStartInfo(FFmpegPath, Parameters);
+
+            ProcessStartInfo oInfo = new System.Diagnostics.ProcessStartInfo(FFmpegPath);
+            oInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            oInfo.Arguments = Parameters;
+
+            oInfo.UseShellExecute = false;
+            oInfo.CreateNoWindow = true;
+            oInfo.RedirectStandardOutput = true;
+            oInfo.RedirectStandardError = true;
+            oInfo.RedirectStandardInput = true;
+            //创建一个字符串和StreamReader 用来获取处理结果
+            string output = null; 
+            StreamReader srOutput = null;
+            try
+            {
+                //调用ffmpeg开始处理命令
+                var proc = Process.Start(oInfo);
+                //proc.WaitForExit();
+                //获取输出流
+                srOutput = proc.StandardError;
+                //转换成string
+                output = srOutput.ReadToEnd();
+                //关闭处理程序
+               // proc.Close();
+            }
+            catch (Exception ex)
+            {
+                output = ex.ToString();
+            }
+            finally
+            {
+                //释放资源
+                if (srOutput != null)
+                {
+                    srOutput.Close();
+                    srOutput.Dispose();
+                }
+            }
+            return output;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            FFmpegPath = "C:/ffmpeg/bin/ffmpeg.exe";
+            //非MP4 MKV字幕方法
+            //ffplay -vf subtitles=subtitle.ass input.mp4
+            //-i D:/Michaux/FFTest/aaa.mp4 -vf subtitles=rain.srt output.mp4
+           // string para = "-i D:/Michaux/FFTest/aaa.mp4 -vf subtitles=rain.srt output.mp4";
+
+
+            string para = "-i D:/Michaux/FFTest/rain.mp4 -vf \"subtitles='D\\://Michaux//FFTest//rain.srt'\" output.mp4";
+            Console.WriteLine(para);
+           // string para = "-i D:/Michaux/FFTest/aaa.mp4 -f srt -i D:/Michaux/FFTest/rain.srt -c:v copy -c:a  -c:s mov_text copy zimu.mp4";
+
+           // string para = "-i D:/Michaux/FFTest/aaa.mp4 -i D:/Michaux/FFTest/rain.srt -map 0:v -map 0:a -map 1:s -c:v copy -c:a copy -c:s mov_text -movflags +faststart zimu.mp4";
+
+
+            // MP4 zimu ="-i  D:/Michaux/FFTest/aaa.mp4 -i D:/Michaux/FFTest/rain.srt -map 0:v -map 0:a -map 1:s -c:v copy -c:a copy -c:s mov_text -movflags +faststart zimu.mp4";
+
+            this.text2.Text = this.text2.Text + RunProcess(para); 
+        }
+        /// <summary>
+        /// 动态文字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn4_Click(object sender, EventArgs e)
+        { 
+            FFmpegPath = "C:/ffmpeg/bin/ffplay.exe";
+            string para = " -i D:/Michaux/FFTest/rain.mp4 -vf drawtext=\"fontfile=arial.ttf:text='Michaux Test':x=w-t*50:fontcolor=darkorange:fontsize=30\"  dongtaizimu.mp4";
+            this.text2.Text = this.text2.Text + RunProcess(para); 
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            FFmpegPath = "C:/ffmpeg/bin/ffmpeg.exe";
+            string para = " -y -i D:/Michaux/FFTest/img.mp4  -r 15 -q:v 2 D:/Michaux/FFTest/model/model1/image%d.jpg";
+            this.text2.Text = this.text2.Text + RunProcess(para); 
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        { 
+            FFmpegPath = "C:/ffmpeg/bin/ffmpeg.exe";
+            string para = " -y -f image2 -i D:/Michaux/FFTest/model/model1/image%d.jpg -vcodec libx264 -r 24  D:/Michaux/FFTest/imgp4.mp4";
+            this.text2.Text = this.text2.Text + RunProcess(para); 
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+          //  overlay=x=main_w-overlay_w-10:y=main_h-overlay_h-10
+            FFmpegPath = "C:/ffmpeg/bin/ffmpeg.exe";
+            string para = " -y -i D:/Michaux/FFTest/model/model1/image1.jpg  -i D:/Michaux/FFTest/ali.jpg -filter_complex \"overlay=122:87\"   D:/Michaux/FFTest/img1.jpg";
+            this.text2.Text = this.text2.Text + RunProcess(para); 
+        }
+        
+    }
+}
